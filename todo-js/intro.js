c1886eb0cefc8f7e4b9a3e22eeb7d630b5ec128c
JayCraft <joshuahon7@gmail.com>
2026-05-29 09:46:59 +0100

    fix vite config

--- todo-js/intro.js
+++ todo-js/intro.js
@@ -1,4 +1,3 @@
-
 let renderIntroHTML = '';
 
 function renderIntroPage() {
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        registration: resolve(__dirname, 'registration.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        profile: resolve(__dirname, 'profile.html'),
+      }
+    }
+  }
 })
